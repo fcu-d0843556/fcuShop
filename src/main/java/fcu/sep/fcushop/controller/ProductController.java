95836c0e4e4cd11ae79da71cfd1bf8fd0988619a
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2021-11-15 14:31:21 +0800

    add 2021/11/15 files include database

---
+++ src/main/java/fcu/sep/fcushop/controller/ProductController.java
@@ -0,0 +1,31 @@
+package fcu.sep.fcushop.controller;
+
+import fcu.sep.fcushop.model.Product;
+import fcu.sep.fcushop.service.ProductService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * this is ProductController.
+ */
+
+@RestController
+public class ProductController {
+
+  @Autowired
+  ProductService productManager;
+
+  @GetMapping("/products")
+  public List<Product> getProducts() {
+    return productManager.getProducts();
+  }
+
+  @PostMapping("/products")
+  public List<Product> getProducts(@RequestParam("keyword") String keyword) {
+    return productManager.getProducts(keyword);
+  }
+}
